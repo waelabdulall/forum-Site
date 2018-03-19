6f450de561088da2993394e4b7de1199fd17b471
Wael Mohamed Abdulaal <waelabdulall@gmail.com>
2018-03-19 17:18:02 +0300

    Add the solution comments

--- ATM Solution/atm.py
+++ ATM Solution/atm.py
@@ -4,4 +4,33 @@ money = 500
 
 request = 277
 
-# 100 + 100 + 50 + 10 + 10 + 5 + 2 = 277
+''' 
+Result:
+	100 + 100 + 50 + 10 + 10 + 5 + 2 = 277
+
+Output :
+	give 100
+	give 100
+	give 50
+	give 10
+	give 10
+	give 5
+	give 2
+
+Hint:
+	while request > 0:
+'''
+
+# define the allowed papers
+
+
+#get the request to match the conditions
+
+
+#in a loop
+
+
+#get the request mod divided by the biggest allowed number to get remain
+
+
+#check if remain equal to the same number and if not check next 
