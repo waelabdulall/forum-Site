b5346acdf51e1dda3cccc43231dcd08b5ef204a6
Wael Mohamed Abdulaal <waelabdulall@gmail.com>
2018-03-20 15:40:43 +0300

    Create new atm_refactored file

---
+++ ATM Solution/atm_refactored.py
@@ -0,0 +1,48 @@
+# allowed papers: 100, 50, 10, 5, and cents
+
+# balance = 500
+
+# request = 275
+
+def withdraw(balance, request):
+
+
+    if   request > balance:
+        print("Can't give you all this money !!")
+
+    elif request < 0:
+        print("More than zero plz!")
+
+    else:
+        # print "Current balance = "+ str(balance-request)
+        while request > 0:
+
+            if request >= 100:
+                request -= 100
+                print ("give 100")
+
+            elif request >= 50:
+                request -= 50
+                print("give 50")
+
+            elif request >= 10:
+                request -= 10
+                print("give 10")
+
+            elif request >= 5:
+                request -= 5
+                print("give 5")
+
+            elif request < 5:
+                print("give " + str(request))
+                request = 0
+
+    return balance-request
+
+balance = 500
+
+balance = withdraw(balance, 277)
+balance = withdraw(balance, 30)
+balance = withdraw(balance, 5)
+balance = withdraw(balance, 500)
+# withdraw(500,270)
