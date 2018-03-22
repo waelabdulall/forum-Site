064fd7f7d60fec1d12b5b64cc4030be24d68bb9c
Wael Mohamed Abdulaal <waelabdulall@gmail.com>
2018-03-22 16:23:11 +0300

    Rename topics to post

--- Forums/main.py
+++ Forums/main.py
@@ -3,13 +3,14 @@ import models
 member1 = models.Member("Wael Abdulaal",38)
 member2 = models.Member("Khalid Mohammed", 30)
 
-topic1 = models.Post("Unix","is a family of multitasking, multiuser computer operating systems that derive from the original AT&T Unix, development starting in the 1970s ")
-topic2 = models.Post("Linux","is a family of free and open-source software operating systems built around the Linux kernel")
-
+post1 = models.Post("Unix","is a family of multitasking, multiuser computer operating systems that derive from the original AT&T Unix, development starting in the 1970s ")
+post2 = models.Post("Linux","is a family of free and open-source software operating systems built around the Linux kernel")
+post3 = models.Post("macOS","is a series of graphical operating systems developed and marketed by Apple Inc. since 2001")
 
 print member1.age
 print member2.name
 
-print topic1.title
-print topic2.content
+print post1.title
+print post2.content
+print post3.title," | " ,post3.content
 
