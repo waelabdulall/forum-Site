5a7b4ffd8bb8b16d9cdd2010374de56adec2353a
Wael Mohamed Abdulaal <waelabdulall@gmail.com>
2018-03-22 11:54:25 +0300

    Remove unnecessary parentheses

--- ATM Solution/atm_refactored.py
+++ ATM Solution/atm_refactored.py
@@ -14,10 +14,10 @@ class ATM:
 	    print '=================================='
 	
 	    if   request > self.balance:
-	        print("Can't give you all this money !!")
+	        print "Can't give you all this money !!"
 	
 	    elif request < 0:
-	        print("More than zero plz!")
+	        print "More than zero plz!"
 	
 	    else:
 	        self.withdrawals_list.append(request)
@@ -27,22 +27,22 @@ class ATM:
 	
 	            if request >= 100:
 	                request -= 100
-	                print ("give 100")
+	                print "give 100"
 	
 	            elif request >= 50:
 	                request -= 50
-	                print("give 50")
+	                print "give 50"
 	
 	            elif request >= 10:
 	                request -= 10
-	                print("give 10")
+	                print "give 10"
 	
 	            elif request >= 5:
 	                request -= 5
-	                print("give 5")
+	                print "give 5"
 	
 	            elif request < 5:
-	                print("give " + str(request))
+	                print "give " + str(request)
 	                request = 0
 	    print '=================================='
 	    return self.balance
