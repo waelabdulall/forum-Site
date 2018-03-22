ddeccabb664b39b2bca4eccf5aab9c5b4ce6b35f
Wael Mohamed Abdulaal <waelabdulall@gmail.com>
2018-03-22 15:56:26 +0300

    Re-syntax for calling the class in the main.py

--- Forums/main.py
+++ Forums/main.py
@@ -1,6 +1,7 @@
 import models
 
-member1 = Member("Wael Abdulaal",38)
-member2 = Member("Khalid Mohammed", 30)
+member1 = models.Member("Wael Abdulaal",38)
+member2 = models.Member("Khalid Mohammed", 30)
 
-print member1.name
+print member1.age
+print member2.name
