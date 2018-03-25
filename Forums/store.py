898006b52927bfa5918fbbd5b141e0bdd3858fd3
Wael Mohamed Abdulaal <waelabdulall@gmail.com>
2018-03-25 09:00:36 +0300

    Add the store for member CRUD class

---
+++ Forums/store.py
@@ -0,0 +1,27 @@
+class MemberStore:
+
+	member = []
+
+	def get_all(self):
+		# to get all /members
+		pass
+
+	def add(self,member):
+		# append name
+		pass
+
+	def get_by_id(self,id):
+		# Search for member by id
+		pass
+
+	def update(self,member):
+		# update member data
+		pass
+
+	def delete(self,id):
+		# delete member by id
+		pass
+
+	def entity_existis(self,member):
+		#checking if antity exists in a store
+		pass
