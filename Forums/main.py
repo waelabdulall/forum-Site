674555f68240b26e2973f6b37bd89d1c1e26bca0
Wael Mohamed Abdulaal <waelabdulall@gmail.com>
2018-03-25 13:43:23 +0300

    Fix to the import method

--- Forums/main.py
+++ Forums/main.py
@@ -1,16 +1,16 @@
-import models
+from models import Member,Post
+from store import MemberStore
 
-member1 = models.Member("Wael Abdulaal",38)
-member2 = models.Member("Khalid Mohammed", 30)
+member1 = Member("Wael Abdulaal",38)
+member2 = Member("Khalid Mohammed", 30)
 
-post1 = models.Post("Unix","Is a family of multitasking, multiuser computer operating systems that derive from the original AT&T Unix, development starting in the 1970s ")
-post2 = models.Post("Linux","Is a family of free and open-source software operating systems built around the Linux kernel")
-post3 = models.Post("macOS","Is a series of graphical operating systems developed and marketed by Apple Inc. since 2001")
+post1 = Post("Unix","Is a family of multitasking, multiuser computer operating systems that derive from the original AT&T Unix, development starting in the 1970s ")
+post2 = Post("Linux","Is a family of free and open-source software operating systems built around the Linux kernel")
+post3 = Post("macOS","Is a series of graphical operating systems developed and marketed by Apple Inc. since 2001")
 
 
-print member1
-print member2
-print
-print post1
-print post2
-print post3
+member_store = MemberStore()
+member_store.add(member1)
+member_store.add(member2)
+
+print member_store.get_all()
