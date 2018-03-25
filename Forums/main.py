be813373c4d014741636a67ded4770fd4f3f7e96
Wael Mohamed Abdulaal <waelabdulall@gmail.com>
2018-03-25 08:35:18 +0300

    Add __str__ in models.py to direct print in main.py

--- Forums/main.py
+++ Forums/main.py
@@ -3,14 +3,14 @@ import models
 member1 = models.Member("Wael Abdulaal",38)
 member2 = models.Member("Khalid Mohammed", 30)
 
-post1 = models.Post("Unix","is a family of multitasking, multiuser computer operating systems that derive from the original AT&T Unix, development starting in the 1970s ")
-post2 = models.Post("Linux","is a family of free and open-source software operating systems built around the Linux kernel")
-post3 = models.Post("macOS","is a series of graphical operating systems developed and marketed by Apple Inc. since 2001")
+post1 = models.Post("Unix","Is a family of multitasking, multiuser computer operating systems that derive from the original AT&T Unix, development starting in the 1970s ")
+post2 = models.Post("Linux","Is a family of free and open-source software operating systems built around the Linux kernel")
+post3 = models.Post("macOS","Is a series of graphical operating systems developed and marketed by Apple Inc. since 2001")
 
-print member1.age
-print member2.name
 
-print post1.title
-print post2.content
-print post3.title," | " ,post3.content
-
+print member1
+print member2
+print
+print post1
+print post2
+print post3
--- Forums/models.py
+++ Forums/models.py
@@ -3,7 +3,13 @@ class Member:
 		self.name = member_name
 		self.age = member_age
 
+	def __str__(self):
+		return "Name: {} | Age: {}".format(self.name,self.age)
+
 class Post:
 	def __init__(self,title,content):
 		self.title = title
 		self.content =content
+
+	def __str__(self):
+		return "Title: {} | {}".format(self.title,self.content)
