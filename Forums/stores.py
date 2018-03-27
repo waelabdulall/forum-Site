ab77ef9dc0e714592faa5975f38a2df17b9c66bb
Wael Mohamed Abdulaal <waelabdulall@gmail.com>
2018-03-27 17:24:05 +0300

    Add pass to the delete function

--- Forums/stores.py
+++ Forums/stores.py
@@ -24,18 +24,15 @@ class MemberStore:
 		return result
 
 	def delete(self,id):
-
-
-
-
+		pass
+		
 class PostStore:
-
 	posts = []
 
 	def add(self,post):
 		# append post
 		self.posts.append(post)
-	
+
 	def get_all(self):
 		# to get all /posts
 		return PostStore.posts
