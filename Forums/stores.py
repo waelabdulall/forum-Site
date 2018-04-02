43bab1d2b2089cc068020d1f69efe22c492f6e04
Wael Mohamed Abdulaal <waelabdulall@gmail.com>
2018-04-02 11:28:57 +0300

    Add __init__ to MemberStore class | remove old code

--- Forums/stores.py
+++ Forums/stores.py
@@ -41,34 +41,8 @@ class MemberStore(BaseStore):
 	members = []
 	last_id = 1
 
-	def add(self,member):
-		member.id = MemberStore.last_id
-		MemberStore.members.append(member)
-		MemberStore.last_id += 1
-	
-	def get_all(self):
-		return MemberStore.members
-
-
-	def get_by_id(self, id):
-		all_members = self.get_all()
-
-		result = None
-
-		for member in all_members:
-			if member.id == id:
-				result = member
-				break
-
-		return result
-
-
-	def entity_exists(self, member):
-
-		result = True
-		if self.get_by_id(member.id) is None:
-			result = False
-		return result
+	def __init__(self):
+		super().__init__(self.member,self.last_id)
 
 
 	def get_by_name(self,name):
@@ -77,20 +51,6 @@ class MemberStore(BaseStore):
 			if member.name == name:
 				yield member
 
-
-	def delete(self,id):
-		member = self.get_by_id(id)
-		MemberStore.members.remove(member)
-
-
-	def update(self,member):
-		
-		all_members = self.get_all()
-		for i, each in enumerate(all_members):
-			if each.id == member.id:
-				all_members[i] = member
-				break
-				
 
 		
 class PostStore(BaseStore):
